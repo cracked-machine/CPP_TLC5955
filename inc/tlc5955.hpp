fe76b3e48b4e13c4445d750223b85ccb90bb39cf
cracked-machine <chrisjsutton@gmail.com>
2021-12-12 03:32:01 +0000

    replaced with bitset code

--- inc/tlc5955.hpp
+++ inc/tlc5955.hpp
@@ -1,6 +1,7 @@
 
 #include <stdint.h>
 #include <bitset>
+#include <array>
 
 #ifdef USE_HAL_DRIVER
     // Required when using GCC 10.3.1 arm-none-eabi 
@@ -22,7 +23,8 @@
 
 namespace tlc5955 {
 
-// https://godbolt.org/z/1q9sn3Gar
+// experimental code can be found here
+// https://godbolt.org/z/7bcsrK3aP
 
 class Driver
 {
@@ -40,94 +42,9 @@ public:
     static const uint8_t m_dc_data_resolution {7};
     static const uint8_t m_gs_data_resolution {16};
 
-    void set_control_bit(bool ctrl);
-
-    void set_ctrl_cmd_bits();
-
-    void set_padding_bits();
-
-    // @brief Set the Function Control (FC) data latch.
-    // See section 8.3.2.7 "Function Control (FC) Data Latch" (page 23).
-    // https://www.ti.com/lit/ds/symlink/tlc5955.pdf
-    // @param DSPRPT Auto display repeat mode enable bit. When enabled each output repeats the PWM control every 65,536 GSCLKs.
-    // @param TMGRST Display timing reset mode enable bit. When enabled the GS counter resets and outputs forced off at the latch rising edge
-    // for a GS data write
-    // @param RFRESH Data in the common register are copied to the GS data latch and DC data in the control data latch are copied to the DC data latch
-    // at the 65,536th GSCLK after the LAT rising edge for a GS data write.
-    // @param ESPWM When 0, conventional PWM is selected. When 1, Enhanced Spectrum (ES) PWM is selected. See 8.4.4 "Grayscale (GS) Function (PWM Control)"
-    // @param LSDVLT LED short detection (LSD) detection voltage selection bit. When this bit is 0, the LSD voltage is VCC × 70%. 
-    // When this bit is 1, the LSD voltage is VCC × 90%. See 8.3.5 "LED Short Detection (LSD)"
-    void set_function_data(bool DSPRPT, bool TMGRST, bool RFRESH, bool ESPWM, bool LSDVLT);
-  
-    // @brief Write the Global BC (Bright Control) data to the common register.
-    // https://www.ti.com/lit/ds/symlink/tlc5955.pdf
-    // @param blue_value The 7-bit word for blue BC
-    // @param green_value The 7-bit word for green BC
-    // @param red_value The 7-bit word for red BC    
-    void set_bc_data(
-        const std::bitset<m_bc_data_resolution> &blue_value, 
-        const std::bitset<m_bc_data_resolution> &green_value, 
-        const std::bitset<m_bc_data_resolution> &red_value);
-
-    // @brief Write the MC (Max Current) data to the common register
-    // https://www.ti.com/lit/ds/symlink/tlc5955.pdf
-    // @param blue_value The 3-bit word for blue MC
-    // @param green_value The 3-bit word for green MC
-    // @param red_value The 3-bit word for red MC
-    void set_mc_data(
-        const std::bitset<m_mc_data_resolution> &blue_value, 
-        const std::bitset<m_mc_data_resolution> green_value, 
-        const std::bitset<m_mc_data_resolution> &red_value);
-
-    // @brief Write the DC (dot correction) data to the common register for the specified LED
-    // https://www.ti.com/lit/ds/symlink/tlc5955.pdf
-    // @param led_idx The selected LED
-    // @param blue_value The 7-bit word for blue DC
-    // @param green_value The 7-bit word for green DC
-    // @param red_value The 7-bit word for red DC
-    bool set_dc_data(
-        uint8_t led_idx, 
-        const std::bitset<m_dc_data_resolution> &blue_value, 
-        const std::bitset<m_dc_data_resolution> &green_value, 
-        const std::bitset<m_dc_data_resolution> &red_value);
-
-    // @brief Convenience function to set all LEDs to the same DC values
-    // @param blue_value The 7-bit word for blue DC
-    // @param green_value The 7-bit word for green DC
-    // @param red_value The 7-bit word for red DC
-    void set_all_dc_data(
-        const std::bitset<m_dc_data_resolution> &blue_value, 
-        const std::bitset<m_dc_data_resolution> &green_value, 
-        const std::bitset<m_dc_data_resolution> &red_value);
-
-    // @brief Write the GS (Grey Scale) data to the common register for the specified LED
-    // @param led_pos The selected LED
-    // @param blue_value The 16-bit word for blue GS
-    // @param green_value The 16-bit word for green GS
-    // @param red_value The 16-bit word for red GS
-    bool set_gs_data(
-        uint8_t led_idx, 
-        const std::bitset<m_gs_data_resolution> &blue_value, 
-        const std::bitset<m_gs_data_resolution> &green_value, 
-        const std::bitset<m_gs_data_resolution> &red_value);
-
-    // @brief Convenience function to set all LEDs to the same GS values
-    // @param blue_value The 16-bit word for blue GS
-    // @param green_value The 16-bit word for green GS
-    // @param red_value The 16-bit word for red GS
-    void set_all_gs_data(
-        const std::bitset<m_gs_data_resolution> &blue_value, 
-        const std::bitset<m_gs_data_resolution> &green_value, 
-        const std::bitset<m_gs_data_resolution> &red_value);
-
-    // @brief Send the data via SPI bus and toggle the latch pin
-    void send_data();
     
     // @brief Clears (zeroize) the common register and call send_data()
     void flush_common_register();
-
-    // @brief toggle the latch pin terminal
-    void toggle_latch();
 
     // @brief Helper function to print bytes as decimal values to RTT. USE_RTT must be defined.
     void print_common_bits();
@@ -152,19 +69,21 @@ public:
         dot_correct = 54U
     };
 
-    
-
-protected:
-
-    static const uint8_t m_common_reg_size_bytes {97};
-    std::array<uint8_t, m_common_reg_size_bytes> m_common_byte_register{0};
-
-private:
-
-    uint8_t built_in_test_fail {0};
-
     // Bits required for correct control reg size
     static const uint16_t m_common_reg_size_bits {769};
+
+    std::bitset<m_common_reg_size_bits> m_common_bit_register {0};
+
+    template<std::size_t TARGET_SIZE, std::size_t SOURCE_SIZE> 
+    void add_bitset(std::bitset<TARGET_SIZE> &target,  const std::bitset<SOURCE_SIZE> &source, const uint16_t &msb_offset);
+    
+
+    template<std::size_t TARGET_SIZE, std::size_t SOURCE_SIZE> 
+    void bitset_to_bytearray(std::array<uint8_t, TARGET_SIZE> &target_array, const std::bitset<SOURCE_SIZE> &source_bitset);  
+
+    
+    template<std::size_t BITSET_SIZE>
+    void print_bits(std::bitset<BITSET_SIZE> &reg);
 
 
      // @brief The number of daisy chained driver chips in the circuit.
@@ -175,8 +94,6 @@ private:
     
     // @brief The number of LEDs per driver chip
     static const uint8_t m_num_leds_per_chip {16};
-
-
 
     // the size of each common register section
     static const uint8_t m_latch_size_bits {1};                                                                             // 1U
@@ -193,14 +110,41 @@ private:
     };
 
     // the offset of each common register section
-    // static const uint8_t m_latch_offset {0};
-    // static constexpr uint8_t m_ctrl_cmd_offset {static_cast<uint8_t>(m_latch_offset + m_latch_size_bits)};                  // 1U
-    // static constexpr uint8_t m_gs_data_offset {static_cast<uint8_t>(m_ctrl_cmd_offset)};                                    // 9U - used in gs data latch only
-    // static constexpr uint8_t m_padding_offset {static_cast<uint8_t>(m_ctrl_cmd_offset + m_ctrl_cmd_size_bits)};             // 9U - used in ctrl data latch only
-    // static constexpr uint16_t m_func_data_offset {static_cast<uint16_t>(m_padding_offset + m_padding_section_size_bits)};   // 9U
-    // static constexpr uint16_t m_bc_data_offset {static_cast<uint16_t>(m_func_data_offset + m_func_data_section_size_bits)}; // 398U
-    // static constexpr uint16_t m_mc_data_offset {static_cast<uint16_t>(m_bc_data_offset + m_bc_data_section_size_bits)};     // 424U
-    // static constexpr uint16_t m_dc_data_offset {static_cast<uint16_t>(m_mc_data_offset + m_mc_data_section_size_bits)};     // 433U
+    static const uint8_t m_latch_offset {0};
+    static constexpr uint8_t m_ctrl_cmd_offset {static_cast<uint8_t>(m_latch_offset + m_latch_size_bits)};                  // 1U
+    static constexpr uint8_t m_gs_data_offset {static_cast<uint8_t>(m_ctrl_cmd_offset)};                                    // 9U - used in gs data latch only
+    static constexpr uint8_t m_padding_offset {static_cast<uint8_t>(m_ctrl_cmd_offset + m_ctrl_cmd_size_bits)};             // 9U - used in ctrl data latch only
+    static constexpr uint16_t m_func_data_offset {static_cast<uint16_t>(m_padding_offset + m_padding_section_size_bits)};   // 9U
+    static constexpr uint16_t m_bc_data_offset {static_cast<uint16_t>(m_func_data_offset + m_func_data_section_size_bits)}; // 398U
+    static constexpr uint16_t m_mc_data_offset {static_cast<uint16_t>(m_bc_data_offset + m_bc_data_section_size_bits)};     // 424U
+    static constexpr uint16_t m_dc_data_offset {static_cast<uint16_t>(m_mc_data_offset + m_mc_data_section_size_bits)};     // 433U
+
+
+        // @brief Predefined write command.
+    // section 8.3.2.3 "Control Data Latch" (page 21).
+    // section 8.3.2.2 "Grayscale (GS) Data Latch" (page 20).
+    // https://www.ti.com/lit/ds/symlink/tlc5955.pdf
+    std::bitset<8> m_ctrl_cmd {0x96};
+
+
+    static const uint8_t m_common_reg_size_bytes {97};
+    std::array<uint8_t, m_common_reg_size_bytes> m_common_byte_register{0};
+
+protected:
+
+
+private:
+
+    uint8_t built_in_test_fail {0};
+
+
+
+
+
+
+
+ 
+
 
 
     // @brief Helper function to set/clear one bit of one byte in the common register byte array
@@ -214,11 +158,7 @@ private:
 
     const uint8_t  m_latch_delay_ms {1};
 
-    // @brief Predefined write command.
-    // section 8.3.2.3 "Control Data Latch" (page 21).
-    // section 8.3.2.2 "Grayscale (GS) Data Latch" (page 20).
-    // https://www.ti.com/lit/ds/symlink/tlc5955.pdf
-    std::bitset<8> m_ctrl_cmd {0x96};
+
 
     // @brief Predefined flush command
     std::bitset<8> m_flush_cmd {0};
@@ -249,5 +189,70 @@ private:
 #endif	
 
 };
+
+
+template<std::size_t TARGET_SIZE, std::size_t SOURCE_SIZE> 
+void Driver::add_bitset(std::bitset<TARGET_SIZE> &target,  const std::bitset<SOURCE_SIZE> &source, const uint16_t &msb_offset) 
+{
+
+    //std::cout << source << std::endl;
+
+
+    // iterate over the source bitset pattern
+    for (uint16_t idx = 0; idx < source.size(); idx++)
+    {
+        //std::cout << idx << "#:" << source.test(idx) << std::endl;
+        if (source.test(idx))
+        {
+            // start from the common register msb and work backwards towards lsb,
+            // ...minus the offset
+            target.set(msb_offset + (source.size() - 1)  - idx, true);
+        }
+        else
+        {
+            target.set(msb_offset + (source.size() - 1) - idx , false);
+            //target.set(source_msb_idx - idx + msb_offset, false);
+        }
+
+    }
+}
+
+template<std::size_t TARGET_SIZE, std::size_t SOURCE_SIZE> 
+void Driver::bitset_to_bytearray(std::array<uint8_t, TARGET_SIZE> &target_array, const std::bitset<SOURCE_SIZE> &source_bitset)
+{
+    for (uint16_t byte_idx = 0; byte_idx < target_array.size(); byte_idx++)
+    {
+        //std::cout << "Byte #" << byte_idx << std::endl;
+        uint8_t word_size_bits = 8;
+        uint16_t byte_offset = byte_idx * word_size_bits;
+        int8_t bit_offset = word_size_bits - 1;
+        for (uint16_t pattern_idx = byte_offset; pattern_idx < byte_offset + word_size_bits; pattern_idx++)
+        {   
+            if (pattern_idx < source_bitset.size())
+            {
+                target_array[byte_idx] |= (source_bitset.test(pattern_idx) << bit_offset);
+                bit_offset--;
+            }
+        }        
+        
+    }
+}
+
+template<std::size_t BITSET_SIZE>
+void Driver::print_bits(std::bitset<BITSET_SIZE> &reg)
+{
+    UNUSED(reg);
+    #ifdef USE_RTT
+        for (uint16_t idx = 0; idx < reg.size(); idx++)
+        {
+            if (idx % 8 == 0)
+                SEGGER_RTT_printf(0, " ");
+            if (idx % 64 == 0)
+                SEGGER_RTT_printf(0, "\n");            
+            SEGGER_RTT_printf(0, "%u ", +reg.test(idx));
+        }
+    #endif
+}
+
 
 } // tlc5955
