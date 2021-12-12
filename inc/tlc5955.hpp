e9c81fdfd9ed629ba52e22b70e373ce0cd856449
cracked-machine <chrisjsutton@gmail.com>
2021-12-12 15:19:02 +0000

    modified class to use butset_utils

--- inc/tlc5955.hpp
+++ inc/tlc5955.hpp
@@ -3,6 +3,8 @@
 #include <bitset>
 #include <array>
 
+#include <bitset_utils.hpp>
+
 #ifdef USE_HAL_DRIVER
     // Required when using GCC 10.3.1 arm-none-eabi 
     // warning: compound assignment with 'volatile'-qualified left operand is deprecated [-Wvolatile]
@@ -36,54 +38,66 @@ public:
 
     bool start_dma_transmit();
     bool pause_dma_transmit(bool pause);
-
-    static const uint8_t m_bc_data_resolution {7};
-    static const uint8_t m_mc_data_resolution {3};
-    static const uint8_t m_dc_data_resolution {7};
-    static const uint8_t m_gs_data_resolution {16};
-
-    
+   
     // @brief Clears (zeroize) the common register and call send_data()
-    void flush_common_register();
+    void reset();
+    void set_latch_cmd(const bool latch);
+    void set_function_cmd(const bool dsprpt, const bool tmgrst, const bool rfresh, const bool espwm, const bool lsdvlt);
+    void set_global_brightness_cmd(const uint8_t blue, const uint8_t green, const uint8_t red);
+    void set_max_current_cmd(const uint8_t blue, const uint8_t green, const uint8_t red);
+    void set_dot_correction_cmd_all(uint8_t pwm);
+    void set_greyscale_cmd_all(uint16_t pwm);
+
+    void process_register();
 
     // @brief Helper function to print bytes as decimal values to RTT. USE_RTT must be defined.
     void print_common_bits();
 
-    // @brief sections offsets for common register
-    enum byte_offsets {
-        // @brief 1 for control data latch, 0 for greyscale data latch 
-        latch = 0U,
-        // @brief Used in control data latch.
-        ctrl_cmd = 0U,
-        // @brief Used in greyscale data latch. 
-        greyscale = 1U,
-        // @brief Used in control data latch. Don't care bits.
-        padding = 1U,
-        // @brief Used in control data latch.
-        function = 49U,
-        // @brief Used in control data latch.
-        brightness_control = 50U,
-        // @brief Used in control data latch.
-        max_current = 53U,
-        // @brief Used in control data latch.
-        dot_correct = 54U
-    };
+
+
+protected:
+
+
+private:
+
+    embedded_utils::bitset_utils bitsetter;
+
+    // // @brief sections offsets for common register
+    // enum byte_offsets {
+    //     // @brief 1 for control data latch, 0 for greyscale data latch 
+    //     latch = 0U,
+    //     // @brief Used in control data latch.
+    //     ctrl_cmd = 0U,
+    //     // @brief Used in greyscale data latch. 
+    //     greyscale = 1U,
+    //     // @brief Used in control data latch. Don't care bits.
+    //     padding = 1U,
+    //     // @brief Used in control data latch.
+    //     function = 49U,
+    //     // @brief Used in control data latch.
+    //     brightness_control = 50U,
+    //     // @brief Used in control data latch.
+    //     max_current = 53U,
+    //     // @brief Used in control data latch.
+    //     dot_correct = 54U
+    // };
+
+            // @brief Predefined write command.
+    // section 8.3.2.3 "Control Data Latch" (page 21).
+    // section 8.3.2.2 "Grayscale (GS) Data Latch" (page 20).
+    // https://www.ti.com/lit/ds/symlink/tlc5955.pdf
+    std::bitset<8> m_ctrl_cmd {0x96};
+
+    // @brief Predefined flush command
+    std::bitset<8> m_flush_cmd {0};
+
+    static const uint8_t m_common_reg_size_bytes {97};
+    std::array<uint8_t, m_common_reg_size_bytes> m_common_byte_register{0};
 
     // Bits required for correct control reg size
     static const uint16_t m_common_reg_size_bits {769};
 
     std::bitset<m_common_reg_size_bits> m_common_bit_register {0};
-
-    template<std::size_t TARGET_SIZE, std::size_t SOURCE_SIZE> 
-    void add_bitset(std::bitset<TARGET_SIZE> &target,  const std::bitset<SOURCE_SIZE> &source, const uint16_t &msb_offset);
-    
-
-    template<std::size_t TARGET_SIZE, std::size_t SOURCE_SIZE> 
-    void bitset_to_bytearray(std::array<uint8_t, TARGET_SIZE> &target_array, const std::bitset<SOURCE_SIZE> &source_bitset);  
-
-    
-    template<std::size_t BITSET_SIZE>
-    void print_bits(std::bitset<BITSET_SIZE> &reg);
 
 
      // @brief The number of daisy chained driver chips in the circuit.
@@ -94,6 +108,12 @@ public:
     
     // @brief The number of LEDs per driver chip
     static const uint8_t m_num_leds_per_chip {16};
+
+
+    static const uint8_t m_bc_data_resolution {7};
+    static const uint8_t m_mc_data_resolution {3};
+    static const uint8_t m_dc_data_resolution {7};
+    static const uint8_t m_gs_data_resolution {16};
 
     // the size of each common register section
     static const uint8_t m_latch_size_bits {1};                                                                             // 1U
@@ -109,6 +129,8 @@ public:
         m_common_reg_size_bits  - m_latch_size_bits - m_ctrl_cmd_size_bits - m_func_data_section_size_bits - m_bc_data_section_size_bits - m_mc_data_section_size_bits - m_dc_data_section_size_bits
     };
 
+    std::bitset<tlc5955::Driver::m_padding_section_size_bits> m_padding {0x00};
+
     // the offset of each common register section
     static const uint8_t m_latch_offset {0};
     static constexpr uint8_t m_ctrl_cmd_offset {static_cast<uint8_t>(m_latch_offset + m_latch_size_bits)};                  // 1U
@@ -119,52 +141,6 @@ public:
     static constexpr uint16_t m_mc_data_offset {static_cast<uint16_t>(m_bc_data_offset + m_bc_data_section_size_bits)};     // 424U
     static constexpr uint16_t m_dc_data_offset {static_cast<uint16_t>(m_mc_data_offset + m_mc_data_section_size_bits)};     // 433U
 
-
-        // @brief Predefined write command.
-    // section 8.3.2.3 "Control Data Latch" (page 21).
-    // section 8.3.2.2 "Grayscale (GS) Data Latch" (page 20).
-    // https://www.ti.com/lit/ds/symlink/tlc5955.pdf
-    std::bitset<8> m_ctrl_cmd {0x96};
-
-
-    static const uint8_t m_common_reg_size_bytes {97};
-    std::array<uint8_t, m_common_reg_size_bytes> m_common_byte_register{0};
-
-protected:
-
-
-private:
-
-    uint8_t built_in_test_fail {0};
-
-
-
-
-
-
-
- 
-
-
-
-    // @brief Helper function to set/clear one bit of one byte in the common register byte array
-    // @param byte The targetted byte in the common register
-    // @param bit The bit within that byte to be set/cleared
-    // @param _new_value The boolean value to set at the bit target_idx
-    void set_value_nth_bit(uint8_t &byte, uint16_t bit, bool new_value);
-
-    
-    //std::bitset<m_common_reg_size_bits> m_common_bit_register{0};
-
-    const uint8_t  m_latch_delay_ms {1};
-
-
-
-    // @brief Predefined flush command
-    std::bitset<8> m_flush_cmd {0};
-
-    // void enable_spi();
-    // void disable_spi();
 
     // void enable_gpio_output_only();
 #ifdef USE_HAL_DRIVER 
@@ -189,70 +165,6 @@ private:
 #endif	
 
 };
-
-
-template<std::size_t TARGET_SIZE, std::size_t SOURCE_SIZE> 
-void Driver::add_bitset(std::bitset<TARGET_SIZE> &target,  const std::bitset<SOURCE_SIZE> &source, const uint16_t &msb_offset) 
-{
-
-    //std::cout << source << std::endl;
-
-
-    // iterate over the source bitset pattern
-    for (uint16_t idx = 0; idx < source.size(); idx++)
-    {
-        //std::cout << idx << "#:" << source.test(idx) << std::endl;
-        if (source.test(idx))
-        {
-            // start from the common register msb and work backwards towards lsb,
-            // ...minus the offset
-            target.set(msb_offset + (source.size() - 1)  - idx, true);
-        }
-        else
-        {
-            target.set(msb_offset + (source.size() - 1) - idx , false);
-            //target.set(source_msb_idx - idx + msb_offset, false);
-        }
-
-    }
-}
-
-template<std::size_t TARGET_SIZE, std::size_t SOURCE_SIZE> 
-void Driver::bitset_to_bytearray(std::array<uint8_t, TARGET_SIZE> &target_array, const std::bitset<SOURCE_SIZE> &source_bitset)
-{
-    for (uint16_t byte_idx = 0; byte_idx < target_array.size(); byte_idx++)
-    {
-        //std::cout << "Byte #" << byte_idx << std::endl;
-        uint8_t word_size_bits = 8;
-        uint16_t byte_offset = byte_idx * word_size_bits;
-        int8_t bit_offset = word_size_bits - 1;
-        for (uint16_t pattern_idx = byte_offset; pattern_idx < byte_offset + word_size_bits; pattern_idx++)
-        {   
-            if (pattern_idx < source_bitset.size())
-            {
-                target_array[byte_idx] |= (source_bitset.test(pattern_idx) << bit_offset);
-                bit_offset--;
-            }
-        }        
-        
-    }
-}
-
-template<std::size_t BITSET_SIZE>
-void Driver::print_bits(std::bitset<BITSET_SIZE> &reg)
-{
-    UNUSED(reg);
-    #ifdef USE_RTT
-        for (uint16_t idx = 0; idx < reg.size(); idx++)
-        {
-            if (idx % 8 == 0)
-                SEGGER_RTT_printf(0, " ");
-            if (idx % 64 == 0)
-                SEGGER_RTT_printf(0, "\n");            
-            SEGGER_RTT_printf(0, "%u ", +reg.test(idx));
-        }
-    #endif
-}
 
 
 } // tlc5955
--- src/tlc5955.cpp
+++ src/tlc5955.cpp
@@ -5,16 +5,13 @@
 #include <cstring>
 #include <byte_position.hpp>
 
+#include <bitset_utils.hpp>
+
 #ifdef USE_RTT
     #include <SEGGER_RTT.h>
 #endif
-
-
-
 namespace tlc5955 
 {
-
-
 
 bool Driver::start_dma_transmit()
 {
@@ -25,46 +22,86 @@ bool Driver::start_dma_transmit()
         return true;
     #endif
 }
+
+// @brief class to implement TLC5955 LED Driver IC
+// Refer to datasheet - https://www.ti.com/lit/ds/symlink/tlc5955.pdf
+void Driver::reset()
+{
+    m_common_bit_register.reset();
+    m_common_byte_register.fill(0);
+}
+
+void Driver::set_latch_cmd(bool latch)
+{
+    std::bitset<m_latch_size_bits> latch_cmd {latch};
+    bitsetter.add_bitset(m_common_bit_register, latch_cmd, m_latch_offset);
+
+    // if the latch is 1 then we also need to add the ctrl command and the padding
+    // See "8.3.2 Register and Data Latch Configuration" of the datasheet
+    if (latch)
+    {
+        bitsetter.add_bitset(m_common_bit_register, m_ctrl_cmd, m_ctrl_cmd_offset);
+        bitsetter.add_bitset(m_common_bit_register, m_padding, m_padding_offset);
+    }
+}
+
+void Driver::set_function_cmd(const bool dsprpt, const bool tmgrst, const bool rfresh, const bool espwm, const bool lsdvlt)
+{
+    std::bitset<m_func_data_section_size_bits> function_cmd {};
+    function_cmd.set(0, dsprpt);
+    function_cmd.set(1, tmgrst);
+    function_cmd.set(2, rfresh);
+    function_cmd.set(3, espwm);
+    function_cmd.set(4, lsdvlt);
+    bitsetter.add_bitset(m_common_bit_register, function_cmd, m_func_data_offset);
+}
+
+void Driver::set_global_brightness_cmd(const uint8_t blue, const uint8_t green, const uint8_t red)
+{
+    const std::bitset<m_bc_data_resolution> blue_cmd {blue};
+    const std::bitset<m_bc_data_resolution> green_cmd {green};
+    const std::bitset<m_bc_data_resolution> red_cmd {red};
     
-bool Driver::pause_dma_transmit(bool pause)
-{
-    #ifdef USE_HAL_DRIVER 
-        // get the HAL result
-        volatile HAL_StatusTypeDef res {HAL_OK};
-        if (pause) { res = HAL_SPI_DMAPause(&m_spi_interface);  }
-        else {  res = HAL_SPI_DMAResume(&m_spi_interface); }        
-        // return it as bool result
-        if(res == HAL_OK) { return true; }
-        else { return false; }        
-    #else
-        // we don't care about SPI for x86-based unit testing
-        UNUSED(pause);
-        return true;
-    #endif
+    bitsetter.add_bitset(m_common_bit_register, blue_cmd, m_bc_data_offset);
+    bitsetter.add_bitset(m_common_bit_register, green_cmd, m_bc_data_offset + m_bc_data_resolution);
+    bitsetter.add_bitset(m_common_bit_register, red_cmd, m_bc_data_offset + m_bc_data_resolution * 2);
 }
 
-
-
-void Driver::flush_common_register()
+void Driver::set_max_current_cmd(const uint8_t blue, const uint8_t green, const uint8_t red)
 {
-    for (auto &byte : m_common_byte_register)
+    const std::bitset<m_mc_data_resolution> blue_cmd {blue};
+    const std::bitset<m_mc_data_resolution> green_cmd {green};
+    const std::bitset<m_mc_data_resolution> red_cmd {red};
+
+    bitsetter.add_bitset(m_common_bit_register, blue_cmd, m_mc_data_offset);
+    bitsetter.add_bitset(m_common_bit_register, green_cmd, m_mc_data_offset + m_mc_data_resolution);
+    bitsetter.add_bitset(m_common_bit_register, red_cmd, m_mc_data_offset + m_mc_data_resolution * 2);
+}
+
+void Driver::set_dot_correction_cmd_all(uint8_t pwm)
+{
+    const std::bitset<m_dc_data_resolution> dc_pwm_cmd {pwm};
+    for (uint8_t dc_idx = 0; dc_idx < 48; dc_idx++)
+	{
+		bitsetter.add_bitset(m_common_bit_register, dc_pwm_cmd, m_dc_data_offset + m_dc_data_resolution * dc_idx);		
+	}
+}
+
+void Driver::set_greyscale_cmd_all(uint16_t pwm)
+{
+    const std::bitset<m_gs_data_resolution> gs_pwm_cmd {pwm}; 
+    for (uint16_t gs_idx = 0; gs_idx < 48; gs_idx++)
     {
-        byte = 0x00;
-    }
-    // send_data();
+    	bitsetter.add_bitset(m_common_bit_register, gs_pwm_cmd, m_gs_data_offset + m_gs_data_resolution * gs_idx);
+    }    
 }
 
-void Driver::print_common_bits()
+void Driver::process_register()
 {
-#ifdef USE_RTT
-    SEGGER_RTT_printf(0, "\r\n");
-    for (uint16_t idx = 0; idx < 8; idx++)
-    {
-        SEGGER_RTT_printf(0, "%u ", +m_common_byte_register[idx]);
-    }
-#endif
-
+    bitsetter.bitset_to_bytearray(m_common_byte_register, m_common_bit_register);
 }
+
+
 
 
 } // namespace tlc5955
